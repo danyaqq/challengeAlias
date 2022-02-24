5257ac34afc76265435c05a738e46ef4ddfa3bec
danyaqq <fedak220@gmail.com>
2022-02-24 18:37:27 +0300

    add teams screen

--- Alias/Service/NetworkService.swift
+++ Alias/Service/NetworkService.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-protocol JokeManagerDelegate {
+protocol JokeManagerDelegate: AnyObject {
     func didUpdateJoke(_ jokeManager: JokeManager, joke: Joke)
 }
 
@@ -15,7 +15,7 @@ struct JokeManager {
     
     let jokeURL = "http://rzhunemogu.ru/RandJSON.aspx?CType=1"
     
-    var delegate: JokeManagerDelegate?
+    weak var delegate: JokeManagerDelegate?
     
     func performRequest(with jokeURL: String) {
         if let url = URL(string: jokeURL) {
@@ -47,43 +47,3 @@ struct JokeManager {
     }
 }
 
-
-
-
-
-
-
-
-
-
-class NetworkService{
-    
-    static let shared = NetworkService()
-    
-    let jokeURL = "http://rzhunemogu.ru/RandJSON.aspx?CType=1"
-    
-    private init() {}
-    
-    func getDataFromAPI<T: Decodable>(url: String, completion: @escaping(T) -> Void){
-        guard let url = URL(string: url) else {
-            return
-        }
-        let task = URLSession.shared.dataTask(with: url){ data, _, error in
-            if let error = error {
-                print(error.localizedDescription)
-            }
-            
-            guard let data = data else {
-                return
-            }
-
-            do{
-                let decodedData = try JSONDecoder().decode(T.self, from: data)
-                completion(decodedData)
-            } catch let err{
-                print(err.localizedDescription)
-            }
-        }
-        task.resume()
-    }
-}
