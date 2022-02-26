48f5f85d48c328c4f38a61544f6efa3b081f0301
danyaqq <fedak220@gmail.com>
2022-02-26 12:16:59 +0300

    fix

--- Alias/Controllers/MainViewController.swift
+++ Alias/Controllers/MainViewController.swift
@@ -9,18 +9,30 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    //MARK: - Views
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var howToPlayButton: UIButton!
+    @IBOutlet weak var teamsCount: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
     
+    //MARK: - Properties
+    var gameData: GameData = GameData()
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
 
+    //MARK: - Setup views
     func setupView(){
         startButton.layer.cornerRadius = 8
         howToPlayButton.layer.cornerRadius = 8
+        stepper.minimumValue = 2
     }
 
+    @IBAction func stepperTapped(_ sender: UIStepper) {
+//        teamsCount
+    }
 }
 
